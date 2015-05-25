c66e7fa43e776f329be5e02608688120b731c8c8
Rafael-Cheng <rafaelcheng13@gmail.com>
2015-05-25 22:56:03 +0800

    code...

--- GUICalculator/Calculator.java
+++ GUICalculator/Calculator.java
@@ -2,6 +2,8 @@
  *A simple calculator with GUI
  *@author Rafael Cheng
  *Haven't finished yet
+ *What to do next is make sure that it can calculate continuously.
+ *last change 2015.5.25
  */
 import java.awt.*;
 import java.awt.event.*;
@@ -13,7 +15,11 @@ public class Calculator {
 }
 
 class MyFrame extends Frame {
+	MyFrame ref = this;
 	TextField tf = new TextField(15);
+	String s;
+	int num1, num2, result, operator;
+	boolean flag = false;
 	MyFrame(String s) {
 		super(s);
 		Panel pn1 = new Panel();
@@ -21,7 +27,7 @@ class MyFrame extends Frame {
 		Panel pn3 = new Panel();
 		Panel pn4 = new Panel();
 		final String[] name = {"0", "1", "2", "3", "4", "5", "6", "7", 
-		"8", "9", "=", ".", "EC", "+", "-", "x", "/"};
+		"8", "9", "=", ".", "EC", "+", "-", "x", "/"};//strs for buttons
 		Button[] button = new Button[name.length];
 		for(int i = 0; i < button.length; i++) {
 			button[i] = new Button("" + name[i]);
@@ -53,11 +59,49 @@ class MyFrame extends Frame {
 		}
 	}
 	private class ButtonMonitor implements ActionListener {
+		
 		public void actionPerformed(ActionEvent e) {
 			if(e.getActionCommand().equals("EC")) {
 				tf.setText("0");
 			}else{
-			    tf.setText(e.getActionCommand());
+				s = e.getActionCommand();
+			    tf.setText(s);
+				ref.launch();//Keep running
+			}
+		}
+	}
+	void launch() {//This try&catch is used to distinguish num&operator 
+		try {
+			if(!flag) {
+			    num1 = Integer.parseInt(s);
+			}else {
+				num2 = Integer.parseInt(s);
+			}
+		}catch (Exception e) {
+			if(s.equals("+")) {
+				operator = 1;
+				flag = true; //once an operator occured, alter the value of the othe number.
+			}else if(s.equals("-")) {
+				operator = 2;
+				flag = true;
+			}else if(s.equals("x") || s.equals("X")) {
+				operator = 3;
+				flag =true;
+			}else if(s.equals("\\")) {
+				operator = 4;
+				flag = true;
+			}else if(s.equals("=")) {
+				switch(operator) {
+					case 1:tf.setText("" + (num1 + num2));
+					       break;
+					case 2:tf.setText("" + (num1 - num2));
+					       break;
+					case 3:tf.setText("" + (num1 * num2));
+					       break;
+					case 4:tf.setText("" + (num1 / num2));
+					       break;
+					default:break;
+				}
 			}
 		}
 	}
